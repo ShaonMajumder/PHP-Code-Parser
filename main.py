1cb55544dcab50dc314f0d8bec62a95e2e769e0b
Shaon Majumder <shaon.fdr@robist.com>
2019-04-25 23:20:46 +0600

    remove all comments

---
+++ main.py
@@ -0,0 +1,143 @@
+import codecs
+
+def read_file(filename):
+	with codecs.open(filename, "r", encoding="utf-8") as file_reader:
+		lines = file_reader.readlines()
+	return lines
+
+def get_single_line_comment(line):
+	if '//' in line:
+		return line[line.index('//'):]
+	else:
+		return False
+
+		
+
+
+
+def get_single_line_comments(lines):
+	comments_ = []
+	for line in lines:
+		comment = get_single_line_comment(line)
+		if(comment): comments_.append(comment)
+	return comments_
+
+
+
+def get_multi_line_comments(lines):
+	one_multi = ""
+	all_multi = []
+	read = False
+	cursor = -1
+	while(cursor<len(lines)-1):
+		cursor = cursor + 1
+		line = lines[cursor]
+		if '*/' in line:
+			read = False
+			for i in range(len(line)) :
+				if line[i] == '*' and line[i+1] == '/':
+					one_multi = one_multi + line[:i+2]
+					newline = line[i+2:]
+			
+			lines[cursor] = newline
+			cursor = cursor - 1
+			all_multi.append(one_multi)
+			one_multi = ''
+		elif(read):
+			one_multi = one_multi + line
+		elif '/*' in line:
+			read = True
+			for i in range(len(line)) :
+				if line[i] == '/' and line[i+1] == '*':
+					one_multi = one_multi + line[i:]
+
+	return all_multi
+
+
+
+
+
+
+def remove_all_single_comments(lines):
+	clean_code = ''
+	single_line_comments = get_single_line_comments(lines)
+	for line in lines:
+		
+		#single_line_comments deduct  from line 
+		
+		for slc in single_line_comments:
+			if slc in line:
+				line = line.replace(slc,'')
+				break
+		clean_code = clean_code + line
+	return clean_code
+
+def single_line(line,multi_read):
+	clean_line = ''
+	loop = True
+	if '*/' in line:
+		#after */
+		line = line[line.index('*/')+2:]
+		multi_read = False
+	while(loop and not multi_read):
+		if '//' in line and '/*' in line:
+			sli = line.index('//')
+			mli = line.index('/*')
+			if sli < mli:
+				if '\r\n' in line:
+					bl = '\r\n'
+				elif '\n' in line:
+					bl = '\n'
+				line = line.replace( get_single_line_comment(line) ,'')  + bl
+			else:
+				#multi before
+				if '/*' in line and '*/' in line:
+					com = line[line.index('/*'):line.index('*/')+2]
+					line = line.replace(com,'')
+					#again run loop on same line
+				elif '/*' in line:
+					#before /*
+					line = line[:line.index('/*')]
+					#continue to next line # correct
+					multi_read = True
+					loop = False
+		elif '//' in line:
+			if '\r\n' in line:
+				bl = '\r\n'
+			elif '\n' in line:
+				bl = '\n'
+			line = line.replace( get_single_line_comment(line) ,'')  + bl
+		elif '/*' in line:
+			if '/*' in line and '*/' in line:
+				com = line[line.index('/*'):line.index('*/')+2]
+				line = line.replace(com,'')
+				#again run loop on same line
+			elif '/*' in line:
+				#before /*
+				line = line[:line.index('/*')]
+				#continue to next line # correct
+				multi_read = True
+				loop = False
+				
+		
+
+		else:
+			clean_line = line
+			loop = False
+	
+	return clean_line,multi_read
+
+
+
+def remove_all_comments(lines):
+	# escape character not handled
+	multi_read = False
+	clean_code = ''
+	for line in lines:
+		clean_line,multi_read= single_line(line,multi_read)
+		clean_code = clean_code + clean_line
+	return clean_code
+
+lines = read_file('sample.php')
+clean_code = remove_all_comments(lines)
+print(clean_code)
---
+++ sample.php
@@ -0,0 +1,60 @@
+<?php
+6//a/*
+//a/*
+/*
+Usage::
+$MySquery = new MyQueries($conn);
+
+$result = $MySquery->insert('data', array('resource_url','read_count','postkey'), array('url','0','gdfg'));
+
+$result = $MySquery->edit('data',array('read_count','postkey'),array('0','518swb'),"`title` = 'switch to tab id chrome.tabs - Google Search'");
+
+$result = $MySquery->delete("post_keys","`postkey` = 'gdgr'");
+*/dfs/* This is another 
+liine2
+*/7786/*757568*/hello
+
+class MyQueries {
+	//private $conn;
+ 
+	function __construct(mysqli $conn) {
+		$this->conn = $conn;
+	}
+	function test_escape($conn,$arr){
+		$result = array();
+		foreach ($arr as $value) {
+			array_push($result , mysqli_real_escape_string($conn,$value));
+		}	
+		return $result;
+	}
+	function insert($database,$insert_fields,$insert_values){
+		$insert_fields = $this->test_escape($this->conn,$insert_fields);
+		$insert_values = $this->test_escape($this->conn,$insert_values);
+
+		$fields = '(`'.implode("`,`", $insert_fields)."`)";
+		$values = "('".implode("','", $insert_values)."')";
+		$sql = "INSERT INTO `{$database}` {$fields} VALUES {$values}";
+		$result = $this->conn->query($sql);
+		return $result;
+	}
+	function delete($database,$condition) {
+		$sql = "DELETE FROM `{$database}` WHERE {$condition}";
+		$result = $this->conn->query($sql);
+		return $result;
+	}
+	function edit($data,$update_keys,$update_values,$condition) {
+		$update_keys = $this->test_escape($this->conn,$update_keys);
+		$update_values = $this->test_escape($this->conn,$update_values);
+
+		$param_string = "";
+		foreach ($update_keys as $keya => $vala) {
+			$valb = $update_values[$keya];
+			$param_string = $param_string . "`{$vala}` = '{$valb}',";
+		}
+		$param_string = rtrim($param_string,",");
+		$sql = "UPDATE `{$data}` SET {$param_string} WHERE {$condition}";
+		$result = $this->conn->query($sql);
+		return $result;
+	}
+}
+?>
